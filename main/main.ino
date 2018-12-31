a48e9fcfd541fcacb4465723e83c54c35eeac155
ellykhodaie <elahekhodaie@yahoo.com>
2018-12-31 16:05:23 +0330

    tetrisRotation

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,7 @@
-//Including External Libraries
+#include <Time.h>
+#include <TimeLib.h>
 #include <LedControl.h>
+
 
 //Defining Constant Values & Handles
     //Handles
@@ -35,6 +37,12 @@
         Point points[4];
         short type;
     } Block;
+
+    
+
+
+
+int randomNumber;
 
 
 //Global Variables
@@ -73,6 +81,40 @@
         }
 //        ledController.setLed( deviceNumber ,0, 0 , true );
     }
+   void ReachedBottomchunck(){
+     int firstRowCounter = 0;
+      bool reached = false;
+
+      for(int i = 0 ; i < 8 ; i++)
+      if(bottomChunck[i][0] == false)
+      firstRowCounter ++;
+      
+      // bottomChunk is empty  
+      if(firstRowCounter == 8) {  
+          short min = liveBlock.points[0].y;
+      for( int i = 0 ; i < 4 ; i++ ) 
+        min = ( liveBlock.points[i].y < min ) ? liveBlock.points[i].y : min;
+        if(min  == 0 )
+        for(int j = 0 ; j < 4 ; j++)
+        bottomChunck[liveBlock.points[j].x][liveBlock.points[j].y] = true;
+      }
+     
+      else{
+        for(int i = 0 ; i< 4 ; i++){
+        if(bottomChunck[liveBlock.points[i].x][liveBlock.points[i].y] == true) 
+        reached  = true;
+           else 
+           reached = false;
+        }
+        if(reached == true)
+        for(int i = 0 ; i< 4 ; i++)
+        bottomChunck[liveBlock.points[i].x][liveBlock.points[i].y] = true;
+        
+
+      }
+      
+      
+    }
 
     void showGameMap( ) {
         bool map[SCREEN_WIDTH][SCREEN_HEIGHT];
@@ -84,6 +126,7 @@
  
         }
         showScreenForMap(map);
+        
     }
     //Game Logic
     bool isGameFinished( ) { /*  TODO jame live block va bottomChunk kolle safaro por kone */
@@ -112,6 +155,99 @@
     }
 
     void rotateLiveBlock() {
+      // #####
+      if(liveBlock.type == 0) {
+        if(liveBlock.points[0].x == liveBlock.points[1].x) {
+          // amoodi
+          int newY = liveBlock.points[2].y ;
+          for(int i = 0 ; i < 4; i++)
+          liveBlock.points[i].y = newY;
+          int newX = liveBlock.points[2].x;
+          liveBlock.points[3].x = newX - 1;
+          liveBlock.points[1].x = newX +1;
+          liveBlock.points[0].x = newX +2;  }
+        else if(liveBlock.points[0].y == liveBlock.points[1].y)  {
+          //ofoghi
+          int newX = liveBlock.points[1].x;
+           for(int i = 0 ; i < 4; i++)
+          liveBlock.points[i].x = newX;
+          int newY = liveBlock.points[1].y;
+          liveBlock.points[0].y = newY + 1;
+          liveBlock.points[2].y = newY -1 ;
+          liveBlock.points[3].y = newY - 2;   } 
+      }
+
+      else if(liveBlock.type == 1)  
+      return;
+
+      else if(liveBlock.type ==2){
+        if((liveBlock.points[0].x == liveBlock.points[3].x )&& (liveBlock.points[0].y >liveBlock.points[1].y))  {
+         int  newX = liveBlock.points[1].x ;
+             liveBlock.points[3].x = newX + 1;
+             liveBlock.points[2].x = newX - 1;
+             liveBlock.points[0].x = newX +1;
+         int  newY = liveBlock.points[1].y;
+          liveBlock.points[3].y = newY++;
+          liveBlock.points[2].y = newY --;
+           liveBlock.points[0].y = newY++;
+           
+        
+        }
+         else
+          return;
+        
+        
+      }
+
+      else if(liveBlock.type == 3) {
+      //#
+     //###
+           int newX =  liveBlock.points[1].x;
+          int newY =  liveBlock.points[1].y;
+        if(liveBlock.points[0].x == liveBlock.points[1].x) {
+          
+          liveBlock.points[3]. x = newX;
+          liveBlock.points[3].y = newY - 1;
+          
+        }
+        else if(liveBlock.points[2].y == liveBlock.points[1].y)
+        { 
+          liveBlock.points[0]. x = newX - 1;
+          liveBlock.points[0].y = newY ;
+          
+        }
+        else if(liveBlock.points[3].x == liveBlock.points[1].x){
+          liveBlock.points[2]. x = newX ;
+          liveBlock.points[2].y = newY  + 1;
+        }
+        else if( liveBlock.points[0].y == liveBlock.points[1].y){
+           liveBlock.points[3]. x = newX + 1;
+          liveBlock.points[3].y = newY ;
+          
+        }
+        
+      }
+
+      else if(liveBlock.type == 4){
+        
+      }
+
+
+      else if(liveBlock.type == 5) {
+        int newX = liveBlock.points[2].x;
+         int newY = liveBlock.points[2].y;
+        
+        if(liveBlock.points[1].y == liveBlock.points[3].y) {
+          liveBlock.points[3].x = newX + 1;
+         liveBlock.points[3].y = newY - 1;
+          
+        }
+        else if(liveBlock.points[0].x == liveBlock.points[3].x){
+            liveBlock.points[3].x = newX - 1;
+         liveBlock.points[3].y = newY + 1;
+        }
+       
+      }
       
     }
 
@@ -137,24 +273,105 @@
     void setup(){
         init_ledController();
         init_buttons();
+        // randomSeed(second());
+//         Serial.begin(9600);
+//          randomSeed(second()); // nothing connected to 0 so read sees noise
+liveBlock.type = 5 ;
 
-        liveBlock.points[0].x = 5;
-        liveBlock.points[1].x = 5;
-        liveBlock.points[2].x = 6;
-        liveBlock.points[3].x = 6;
+     if(liveBlock.type== 0)   {
+      //  #
+     //   #
+      //  #
+    //    #
+        liveBlock.points[0].x = 4;
+        liveBlock.points[1].x = 4;
+        liveBlock.points[2].x = 4;
+        liveBlock.points[3].x = 4;
+        liveBlock.points[0].y = 31;
+        liveBlock.points[1].y = 30;
+        liveBlock.points[2].y = 29;
+        liveBlock.points[3].y = 28;
+     }
+ 
+else if(liveBlock.type == 1){
+  //##
+  //##
+        liveBlock.points[0].x = 3;
+        liveBlock.points[1].x = 3;
+        liveBlock.points[2].x = 4;
+        liveBlock.points[3].x = 4;
+        liveBlock.points[0].y = 31;
+        liveBlock.points[1].y = 30;
+        liveBlock.points[2].y = 31;
+        liveBlock.points[3].y = 30;
+  
+}
+else if(liveBlock.type== 2)
+{
+  //#
+ // ###
+        liveBlock.points[0].x = 3;
+        liveBlock.points[1].x = 4;
+        liveBlock.points[2].x = 5;
+        liveBlock.points[3].x = 3;
+        liveBlock.points[0].y = 31;
+        liveBlock.points[1].y = 30;
+        liveBlock.points[2].y = 30;
+        liveBlock.points[3].y = 30;
+
+}
+
+else if(liveBlock.type == 3){
+  // #
+  //###
+
+        liveBlock.points[0].x = 4;
+        liveBlock.points[1].x = 4;
+        liveBlock.points[2].x = 5;
+        liveBlock.points[3].x = 3;
 
         liveBlock.points[0].y = 31;
         liveBlock.points[1].y = 30;
         liveBlock.points[2].y = 30;
-        liveBlock.points[3].y = 29;
+        liveBlock.points[3].y = 30;
+
+  
+}
+else if(liveBlock.type == 4){
+  //  #
+ // ###
+        liveBlock.points[0].x = 5;
+        liveBlock.points[1].x = 4;
+        liveBlock.points[2].x = 5;
+        liveBlock.points[3].x = 3;
+        liveBlock.points[0].y = 31;
+        liveBlock.points[1].y = 30;
+        liveBlock.points[2].y = 30;
+        liveBlock.points[3].y = 30;
+}
+else if(liveBlock.type == 5){
+  //##
+  // ##
+
+        liveBlock.points[0].x = 5;
+        liveBlock.points[1].x = 4;
+        liveBlock.points[2].x = 4;
+        liveBlock.points[3].x = 3;
+        liveBlock.points[0].y = 30;
+        liveBlock.points[1].y = 31;
+        liveBlock.points[2].y = 30;
+        liveBlock.points[3].y = 31;
+}
     }
 
     void loop(){ 
+      randomNumber = random(0,second());
+      printf("%d " ,randomNumber); 
         showGameMap();
         getAction();
         moveLiveBlockDown();
         delay(500);
+         
         
-
     }
 
